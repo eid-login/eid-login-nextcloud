0d53442f7d1a2f997b67880ca19e1ed0d6554f7e
Michael Rauh <michael.rauh@ecsec.de>
2022-10-12 15:57:49 +0200

    Use and migrate to cypress 10

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  watchForFileChanges: false,
+  chromeWebSecurity: false,
+  pageLoadTimeout: 40000,
+  defaultCommandTimeout: 20000,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://nextcloud24.p396.de',
+  },
+})
